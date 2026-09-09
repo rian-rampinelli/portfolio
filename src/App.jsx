3765f924d95686aa9b98ed67c2ce9a73a5d55f16
Rian <riaann.barbosa@gmail.com>
2026-09-08 23:12:31 -0300

    feat: atualiza navbar, contatos e tecnologias

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
-import { FaJava, FaReact, FaDocker, FaGithub, FaLinkedin, FaGit, FaNode } from "react-icons/fa";
-import { SiSpringboot, SiPostgresql,SiJunit5  } from "react-icons/si";
+import { FaJava, FaReact, FaDocker, FaGithub, FaGitAlt, FaNode,FaLinkedin } from "react-icons/fa";
+import { SiPostgresql,SiJunit5,SiGmail,SiGitlab    } from "react-icons/si";
+import { BiLogoSpringBoot } from "react-icons/bi";
 
 
 function App() {
@@ -9,22 +10,21 @@ function App() {
             <div className="nav">
                 <p className="name">Rian <span className="sobrenome">Rampinelli</span> </p>
                 <ul className="nav-links">
-                    <li>Apresentaçaõ</li>
-                    <li>Sobre mim</li>
-                    <li>Tecnologias</li>
-                    <li>Projetos</li>
-                    <li>Contato</li>
+                    <a href="#header">Sobre mim</a>
+                    <a href="#tecnologias">Tecnologias</a>
+                    <a href="#projetos">Projetos</a>
+                    <a href="#contatos">Contato</a>
                 </ul>
             </div>
             <div className="container">
 
-                <section style={{ marginTop: 120 }}>
-                    <header className="header">
+                <div style={{ marginTop: 120 }}>
+                    <section id="header">
                         <div></div>
                         <p className="header-title">Olá, meu nome é </p>
-                        <p className="header-name">rian rampinelli</p>
+                        <p className="header-name">Rian Rampinelli</p>
                         <p className="header-title">Desenvolvedor full stack</p>
-                    </header>
+                    </section>
                     <div className="banner">
                         <h2>Sobre mim</h2>
                         <p className="text-banner">Desenvolvedor Backend especializado em Java e Spring, com foco na construção de APIs robustas, escaláveis e de alta performance. Tenho experiência no desenvolvimento de aplicações utilizando tecnologias como Java, Spring Boot, Spring Data JPA, Spring Security, Hibernate e PostgreSQL. Sou focado na criação de código limpo e manutenível, aplicação de boas práticas de arquitetura e desenvolvimento de soluções eficientes, seguras e escaláveis, buscando continuamente evoluir a qualidade, performance e confiabilidade das aplicações.
@@ -32,7 +32,7 @@ function App() {
                     </div>
 
 
-                    <div className="tecnologias">
+                    <section id="tecnologias">
 
                         <h2>Tecnologias</h2>
                         <div className="tecnologias-itens">
@@ -41,69 +41,69 @@ function App() {
                                     color: "red",
                                     fill: "#8355e5",
                                 }} />
-                                <p>Java</p>
+                                <p className="tecnologias-itens-card-text">Java</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                <SiSpringboot size={52}  style={{
+                                <BiLogoSpringBoot  size={52}  style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Spring</p>
+                                <p className="tecnologias-itens-card-text">Spring Boot</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                 <FaReact size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>React</p>
+                                <p className="tecnologias-itens-card-text">React</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                  <FaNode size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Node</p>
+                                <p className="tecnologias-itens-card-text">NodeJs</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                  <FaDocker size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Docker</p>
+                                <p className="tecnologias-itens-card-text">Docker</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                  <FaGit size={52} style={{
+                                  <FaGitAlt size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Git</p>
+                                <p className="tecnologias-itens-card-text">Git</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                   <FaGithub size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Github</p>
+                                <p className="tecnologias-itens-card-text">Github</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                   <SiPostgresql size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Postgres</p>
+                                <p className="tecnologias-itens-card-text">Postgres</p>
                             </div >
                             <div className="tecnologias-itens-card">
                                   <SiJunit5 size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }}/>
-                                <p>Junit</p>
+                                <p className="tecnologias-itens-card-text">Junit</p>
                             </div>
                         </div>
 
-                    </div>
+                    </section>
 
-                    <div className="projetos">
+                    <section id="projetos">
                         <h2>projetos</h2>
                         <div className="projetos-itens">
                             <div className="projetos-itens-card">
@@ -139,35 +139,42 @@ function App() {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </section>
 
-                    <div className="contatos">
+                    <section id="contatos">
                         <h2>Contatos</h2>
                         <div className="contatos-itens">
-                            <div className="contatos-itens-card">
-                                icone
-                                <p>gmail</p>
-                            </div>
-                            <div className="contatos-itens-card">
-                                icone
-                                <p>github</p>
-                            </div>
-                            <div className="contatos-itens-card">
-                                icone
-                                <p>gitlab</p>
-                            </div>
-                            <div className="contatos-itens-card">
-                                icone
-                                <p>linkedin</p>
-                            </div>
-                            <div className="contatos-itens-card">
-                                icone
-                                <p>whattsapp</p>
-                            </div>
-
+                            <a href="https://www.linkedin.com/in/rian-rampinelli" target="_blank"  className="contatos-itens-card">
+                                  <FaLinkedin size={48} style={{
+                                    color: "red",
+                                    fill: "#8355e5",
+                                }}/>
+                                <p className="tecnologias-itens-card-text">Linkedin</p>
+                            </a>
+                            <a href="https://github.com/rian-rampinelli" target="_blank" className="contatos-itens-card" >
+                                  <FaGithub size={48} style={{
+                                    color: "red",
+                                    fill: "#8355e5",
+                                }}/>
+                                <p className="tecnologias-itens-card-text">Github</p>
+                            </a>
+                            <a  href="https://gitlab.com/riangkmc" target="_blank"  className="contatos-itens-card">
+                                  <SiGitlab size={48} style={{
+                                    color: "red",
+                                    fill: "#8355e5",
+                                }}/>
+                                <p className="tecnologias-itens-card-text">GitLab</p>
+                            </a>
+                           <a   href="https://mail.google.com/mail/?view=cm&fs=1&to=riaann.barbosa@gmail.com" target= "_blank" className="contatos-itens-card">
+                                  <SiGmail size={48} style={{
+                                    color: "red",
+                                    fill: "#8355e5",
+                                }}/>
+                                <p className="tecnologias-itens-card-text">Gmail</p>
+                            </a>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
 
             </div>
 
