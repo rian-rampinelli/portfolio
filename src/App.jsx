8ac90b5509e9ac6ef72092b1d11edde2bb8ff060
Rian <riaann.barbosa@gmail.com>
2026-09-10 05:36:16 -0300

    feat: finaliza cards e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css"
-import { FaJava, FaReact, FaDocker, FaGithub, FaGitAlt, FaNode,FaLinkedin } from "react-icons/fa";
-import { SiPostgresql,SiJunit5,SiGmail,SiGitlab    } from "react-icons/si";
+import { FaJava, FaReact, FaDocker, FaGithub, FaGitAlt, FaNode, FaLinkedin, FaJs } from "react-icons/fa";
+import { SiPostgresql, SiJunit5, SiGmail, SiGitlab, SiVite } from "react-icons/si";
 import { BiLogoSpringBoot } from "react-icons/bi";
+import projeto1 from "./assets/projeto1.png";
 
 
 function App() {
@@ -37,66 +38,66 @@ function App() {
                         <h2>Tecnologias</h2>
                         <div className="tecnologias-itens">
                             <div className="tecnologias-itens-card">
-                                <FaJava size={52}  style={{
+                                <FaJava size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
                                 }} />
                                 <p className="tecnologias-itens-card-text">Java</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                <BiLogoSpringBoot  size={52}  style={{
+                                <BiLogoSpringBoot size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Spring Boot</p>
                             </div>
                             <div className="tecnologias-itens-card">
                                 <FaReact size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">React</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                 <FaNode size={52} style={{
+                                <FaNode size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">NodeJs</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                 <FaDocker size={52} style={{
+                                <FaDocker size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Docker</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                  <FaGitAlt size={52} style={{
+                                <FaGitAlt size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Git</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                  <FaGithub size={52} style={{
+                                <FaGithub size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Github</p>
                             </div>
                             <div className="tecnologias-itens-card">
-                                  <SiPostgresql size={52} style={{
+                                <SiPostgresql size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Postgres</p>
                             </div >
                             <div className="tecnologias-itens-card">
-                                  <SiJunit5 size={52} style={{
+                                <SiJunit5 size={52} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Junit</p>
                             </div>
                         </div>
@@ -104,72 +105,79 @@ function App() {
                     </section>
 
                     <section id="projetos">
-                        <h2>projetos</h2>
+                        <h2>Projetos</h2>
                         <div className="projetos-itens">
-                            <div className="projetos-itens-card">
-                                <div>imagem</div>
-                                <div>
-                                    <h3>Wiki of Westeros</h3>
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet harum maiores blanditiis eligendi atque id vero dolor commodi non! Aliquam velit ut est dolores officiis quo rerum ratione debitis!</p>
-                                    <p><link rel="stylesheet" href="" />entrar</p>
+                            <div className="projetos-card">
+                                <div className="card-image">
+                                    <img src={projeto1} alt="Projeto 1" />
+                                    <div className="card-technologies">
+                                        <span className="technology">
+                                            <FaReact size={32
+                                            } style={{
+                                                color: "red",
+                                                fill: "#8355e5",
+                                            }} />
+                                        </span>
+                                        <span className="technology">
+                                            <FaJs size={32} style={{
+                                                color: "red",
+                                                fill: "#8355e5",
+                                            }} />
+                                        </span>
+                                        <span className="technology">
+                                            <SiVite size={32
+                                            } style={{
+                                                color: "red",
+                                                fill: "#8355e5",
+                                            }} />
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="projetos-itens-card">
-                                <div>imagem</div>
-                                <div>
+
+                                <div className="card-content">
                                     <h3>Wiki of Westeros</h3>
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet harum maiores blanditiis eligendi atque id vero dolor commodi non! Aliquam velit ut est dolores officiis quo rerum ratione debitis!</p>
-                                    <p><link rel="stylesheet" href="" />entrar</p>
+                                    <p>
+                                        Uma wiki sobre o universo de Game of Thrones.
+                                    </p>
+                                    <a className="card-button" href="https://wiki-of-westeros.vercel.app/" target="_blank">Visitar</a>
                                 </div>
+
                             </div>
-                            <div className="projetos-itens-card">
-                                <div>imagem</div>
-                                <div>
-                                    <h3>Wiki of Westeros</h3>
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet harum maiores blanditiis eligendi atque id vero dolor commodi non! Aliquam velit ut est dolores officiis quo rerum ratione debitis!</p>
-                                    <p><link rel="stylesheet" href="" />entrar</p>
-                                </div>
-                            </div>
-                            <div className="projetos-itens-card">
-                                <div>imagem</div>
-                                <div>
-                                    <h3>Wiki of Westeros</h3>
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet harum maiores blanditiis eligendi atque id vero dolor commodi non! Aliquam velit ut est dolores officiis quo rerum ratione debitis!</p>
-                                    <p><link rel="stylesheet" href="" />entrar</p>
-                                </div>
-                            </div>
+
+
+
                         </div>
                     </section>
 
                     <section id="contatos">
                         <h2>Contatos</h2>
                         <div className="contatos-itens">
-                            <a href="https://www.linkedin.com/in/rian-rampinelli" target="_blank"  className="contatos-itens-card">
-                                  <FaLinkedin size={48} style={{
+                            <a href="https://www.linkedin.com/in/rian-rampinelli" target="_blank" className="contatos-itens-card">
+                                <FaLinkedin size={48} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Linkedin</p>
                             </a>
                             <a href="https://github.com/rian-rampinelli" target="_blank" className="contatos-itens-card" >
-                                  <FaGithub size={48} style={{
+                                <FaGithub size={48} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Github</p>
                             </a>
-                            <a  href="https://gitlab.com/riangkmc" target="_blank"  className="contatos-itens-card">
-                                  <SiGitlab size={48} style={{
+                            <a href="https://gitlab.com/riangkmc" target="_blank" className="contatos-itens-card">
+                                <SiGitlab size={48} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">GitLab</p>
                             </a>
-                           <a   href="https://mail.google.com/mail/?view=cm&fs=1&to=riaann.barbosa@gmail.com" target= "_blank" className="contatos-itens-card">
-                                  <SiGmail size={48} style={{
+                            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=riaann.barbosa@gmail.com" target="_blank" className="contatos-itens-card">
+                                <SiGmail size={48} style={{
                                     color: "red",
                                     fill: "#8355e5",
-                                }}/>
+                                }} />
                                 <p className="tecnologias-itens-card-text">Gmail</p>
                             </a>
                         </div>
