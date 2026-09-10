8b583903181a6e525710ac886afa1f6ec7bc78f5
Rian <riaann.barbosa@gmail.com>
2026-09-10 20:43:00 -0300

    feat: inicia menu hamburguer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import '../NavBar/NavBar.css'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 function NavBar() {
+
+    const [open, setOpen] = useState(false)
+
+ 
+
     return (
         <div className="nav">
             <p className="name">Rian <span className="sobrenome">Rampinelli</span> </p>
@@ -10,6 +18,46 @@ function NavBar() {
                 <a href="#projetos">Projetos</a>
                 <a href="#contatos">Contato</a>
             </ul>
+            <button
+                className="hamburger"
+                onClick={() => setOpen(!open)}
+
+            >
+                <GiHamburgerMenu size={52} style={{
+                    color: "red",
+                    fill: "#8355e5",
+                }} ></GiHamburgerMenu>
+            </button>
+
+            {open && (
+                <div className="modal">
+                    <div style={{display:"flex",gap:50}}>
+                        <p className="name">Rian <span className="sobrenome">Rampinelli</span> </p>
+
+                        <button
+                            className="hamburger"
+                            onClick={() => setOpen(!open)}
+
+                        >
+                            <IoMdClose size={52} style={{
+                                color: "red",
+                                fill: "#8355e5",
+                            }} ></IoMdClose>
+                        </button>
+                    </div>
+
+
+                    <div className='nav-links-modal'>
+                        <a href="#header">Sobre mim</a>
+                        <a href="#tecnologias">Tecnologias</a>
+                        <a href="#projetos">Projetos</a>
+                        <a href="#contatos">Contato</a>
+                    </div>
+
+                </div>
+
+            )}
+
         </div>
     )
 }
